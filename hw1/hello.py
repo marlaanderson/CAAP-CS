4050b1e78c68fccaee1ffc46fc98b16893a3ff1e
marlaanderson <mfanderson@uchicago.edu>
2018-07-08 11:21:46 -0500

    Adding hello.py

---
+++ hw1/hello.py
@@ -0,0 +1,8 @@
+# Prints "Hello!" to screen.
+def hello_world ():
+    print ("Hello!")
+    name = input ("What is your name?")
+    color = input ("What is your favorite color>")
+    ice_cream = input ("What is your favorite ice cream?")
+    print ("Your name is", name , "and your favorite color is", color, ", and your favorite ice cream is", ice_cream,".")
+hello_world ()
