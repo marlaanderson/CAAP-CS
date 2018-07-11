aa448c82c6bebfe3fb22ed7bd8922ae893bba196
marlaanderson <mfanderson@uchicago.edu>
2018-07-11 13:40:40 -0500

    adding final project

--- hw1/seconds.py
+++ hw1/seconds.py
@@ -1,6 +1,11 @@
 #Converts years into seconds
 def seconds_alive ():
+    SecondsPerMinute = 60
+    MinutesPerHour = 60
+    HoursPerDay = 24
+    DaysPerWeek = 7
+    WeeksPerYear = 52
     YearsAlive = input ("This program converts your age to seconds, and tells you the minimum amount of seconds you have been alive. This is not accurate to the exact second, but is simply an estimation. How old are you?")
-    SecondsAlive = (YearsAlive) * (31449600/2*2)
+    SecondsAlive = SecondsPerMinute*MinutesPerHour*HoursPerDay*DaysPerWeek*WeeksPerYear*YearsAlive
     print ("Wow, congratulations! You've succesfully been alive for at least %s seconds!" %(SecondsAlive))
 seconds_alive ()
