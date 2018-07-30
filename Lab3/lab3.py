6517b2ccf6593f97947e90d90cd45f2bdd95ca1d
marlaanderson <mfanderson@uchicago.edu>
2018-07-30 01:11:08 -0500

    Lab3

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -10,15 +10,10 @@ chutes_ladders = {4 : 7,
 					12 : 2,
 					14: 6}
 
-chutes = {12 : 2, 14 : 6}
-ladders = {4 : 7, 8 : 15}
-
-
 # Rolls a die of six sides and returns the result
 def roll_die():
 	roll = randint(1,6)
-	print (roll)
-roll_die()
+	return roll
 
 # makes a game (just a list) of the given dimensions
 def makeGame(w, l):
@@ -30,22 +25,79 @@ def makeGame(w, l):
 
 # checks if given square is a chutes or ladders
 def is_chutes_ladders(d):
-	if d in chutes:
-		print("Chute") 
-	elif d in ladders:
-		print("Ladder")
+	global chutes_ladders
+	if d in chutes_ladders:
+		return True
 	else:
-		print ("False")
+		return False
 
-is_chutes_ladders(14)
-"""
 # function to make and play a game
-def play_game():
-	state = #This should be a list of dictionaries of each player's state
-	mode = 'pc' # or 'user'
-	raise ValueError("todo")
+def play_game(w,l,mode):
+	global chutes_ladders
+	#i was using this but its uneccesary for my code
+	mode = int(input("Who's playing? 1:pc or 2:user? "))
+	won = 0
+	if mode == 2:
+		game = makeGame(w,l)
+		players = int(input("How many players?"))
+		state = []
+		for i in range(players):
+			player= input("What is player's %s name?" %(i+1))
+			state.append({'player': player, 'state': 1,'moves': 0})
+		#This should be a list of dictionaries of each player's state
+		while won == 0:
+			for i in range(players):
+				state[i]["moves"]+=1
+				input("%s, Press ENTER to roll the die."%(state[i]["player"]))
+				roll = roll_die()
+				print ("%s, you rolled a %s."%(state[i]["player"],roll))
+				#for debugging purposes
+				#print(state[i]['state'])
+				placement = state[i]['state']+roll
+				#for debugging purposes
+				#print (placement) 
+				# this is checking before you add the roll to the position
+				if is_chutes_ladders(placement):
+					state[i]['state'] = chutes_ladders[placement]
+					print ("%s, you are now in square %s." %(state[i]["player"],state[i]['state']))
+				else:
+					state[i]['state'] = placement
+					print ("%s, you are now in square %s." %(state[i]["player"],placement))
+				if (state[i]['state'] >= len(game)):
+					won = 1
+					winner = state[i]["player"]
+					break
+		print("Congratulations, %s, you win. You took %s moves."%(winner,state[i]["moves"]))
+	elif mode == 1:
+		game = makeGame(w,l)
+		players = int(input("How many players?"))
+		state = []
+		for i in range(players):
+			state.append({'player': "", 'state': 1,'moves': 0})
+		#This should be a list of dictionaries of each player's state
+		won = 0
+		while won == 0:
+			for i in range(players):
+				state[i]["moves"]+=1
+				roll = roll_die()
+				#for debugging purposes
+				#print(state[i]['state'])
+				placement = state[i]['state']+roll
+				#for debugging purposes
+				#print (placement) 
+				# this is checking before you add the roll to the position
+				if is_chutes_ladders(placement):
+					state[i]['state'] = chutes_ladders[placement]
+				else:
+					state[i]['state'] = placement
+				if (state[i]['state'] >= len(game)):
+					won = 1
+					break
+		print("It took %s moves."%(state[i]["moves"]))
+	else:
+		print ("Please enter an integer: 1 or 2.")
+play_game(4,4,2)
 
 # Runs the game as a simulation and keeps track of the number of moves it takes to win and returns average
 def simulate_game():
-
-"""
+	play_game()
--- Lab3/matrix.py
+++ Lab3/matrix.py
@@ -1,15 +1,42 @@
 # takes the number of rows and columns and makes a matrix of those dimensions
 def make_matrix(r, c)
-	raise ValueError("todo")
+	matrix = []
+    for i in range(r):
+    	list1= []
+		for j in range(c):
+	    	list1.append(0)
+		matrix.append(store)
+    return matrix
 
 # takes two matrices and multiplies them returnin the resulting matrix
 def matrixmult(a,b):
-	raise ValueError("todo")
+    if len(a[0]) == len(b):
+		matrix2 = make_matrix(len(a), len(b[0]))
+		for i in range(len(product)):
+	    	for j in range(len(product[0])):
+	        	variable = 0
+				for n in range(len(b)):
+		    		product[i][j] += float(a[i][n]) * float(b[n][j])
+		return product
+    else:
+		return []
 
 # prints the given matrix, mostly for testing purposes
 def print_matrix(m):
-	raise ValueError("todo")
+	print(matrixmult(a,b))
 
 # given a state matrix, and a transition matrix, runs a markov simulation of the game and returns average number of moves.  
-def markov_simulation():
-	raise ValueError("todo")
+def markov_simulation(initial, transition, simulation):
+	value = 0
+	for i in range(simulation):
+		loop = True
+		moves = 0
+		temp = matrixmult(initial, transition)
+			while loop == True:
+				temp = matrixmult(temp, transition)
+				moves += 1
+				if float(temp[0][11]) > float(0.99):
+					value += moves
+					loop = False
+	avgmoves = float(value)/simulation
+	print("Average moves: %s" %(avgmoves))
